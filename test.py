d1fe3c7832bd069cc73c14011d014eb5559250d8
lvyang <642826959@qq.com>
2021-01-25 15:39:49 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import yagmail
+
+#发送的邮件中带内容，多张图片，多个文件（csv，xls，txt，doc，docx）
+
+def sendmail():
+    yag = yagmail.SMTP(user='1064760814@qq.com', password='snzyxqetuvzmbfjf', host='smtp.qq.com')
+    #拿到你要发送的QQ邮箱的对象
+    #很多情况下，你这个字符串的要用列表去存（方便多个字符串)
+    #yagmail.inline()专门发送制定图片的,你指定图片必须要有路径！
+    # img_dir = 'C:/Users/10647/Desktop/图/微信图片_20201109101810.png'
+    # img_dir2 = 'QQ图片20201108013136.png'
+
+    contents = ['这是一些简单的复试流程与学习规划']
+    # '复试教学计划.docx'
+    # attachments = ['C:/Users/10647/Desktop/图/微信图片_20201109101810.png']
+    persons = [ '642826959@qq.com']
+    yag.send(to=persons, subject='测试发图片与文档邮件', contents=contents)
+
+if __name__ == '__main__':
+    sendmail()
